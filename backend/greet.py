07a38226f9f5a662484c01636c3c7e0864011f13
Von Andrei De Leon <vonnieeeeeee@plv.edu.ph>
2026-03-31 15:24:12 +0800

    wefwe

--- backend/greet.py
+++ backend/greet.py
@@ -4,4 +4,4 @@ def greet_user():
     print(f"Hello, {first_name} {last_name}!")
 
 if __name__ == "__main__":
-    greet_user() #change thsi shetsssssdwd
+    greet_user() #change thsi shetsssssdwdsefef
