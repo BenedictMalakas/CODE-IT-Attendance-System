857dec4dedda277eed1a11aaff060fc63533ed97
BenedictMalakas <malakasjohnbenedict@gmail.com>
2026-05-06 12:26:34 +0800

    Fix database model clash, update settings, and secure permissions

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s5#lxjf)g3*f*@3a0*c*tm968zrkp@*mup$ch@e167d&ccb7*s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['codeit-attendancesystem.me', 'www.codeit-attendancesystem.me', '127.0.0.1', 'localhost']
 
 
 # Application definition
